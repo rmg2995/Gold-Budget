6841cf38fe00782fc40ca4638d6e129e447f9a06
vlee13 <vlee13@gmail.com>
2020-08-24 13:08:49 -0700

    refactored meetteam and form.js

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import TheContext from '../../TheContext'
+import React, { Component } from "react";
+import TheContext from "../../TheContext";
 
 const Profile = (props) => {
-    return (
-        <div>
-            Profile Page
-            <Welcome />  {/*'Look ma!  No props!!!'*/}
-        </div>
-    );
-}
-
+  return (
+    <div>
+      Profile Page
+      <Welcome /> {/*'Look ma!  No props!!!'*/}
+    </div>
+  );
+};
 
 const Welcome = () => {
-    
-    //With Context I can skip the prop drilling and access the context directly 
-    const { user, setUser, history } = React.useContext(TheContext); 
-    
-    return (
-        <div>Welcome {user?.email} <img src={user?.imageUrl} />
-            <button onClick={()=>history.push("/")}>Home</button>
-        </div>
-    )
-}
+  //With Context I can skip the prop drilling and access the context directly
+  const { user, history } = React.useContext(TheContext);
+
+  return (
+    <div>
+      Welcome {user?.email} <img src={user?.imageUrl} />
+      <button onClick={() => history.push("/")}>Home</button>
+    </div>
+  );
+};
 
 export default Profile;
